ec12a853d421c77ca5aeb62685ebf1c2ea0e3458
vanessa2099 <54544288+vanessa2099@users.noreply.github.com>
2019-10-08 22:43:42 -0500

    Tarea 2 de numero par o non.

---
+++ TAREAS/2/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int arc, char const *argu[])
+
+{
+	int x;
+	scanf("%d",&x);
+	if (x%2==0)
+
+		printf("Es un numero par\n");
+	
+	else
+	
+		printf("Es numero non\n");
+
+	return 0;
+}		
