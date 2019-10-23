6802cf920f6270421aa380326305bd2267e35a31
vanessa <vbazaldua60@gmail.com>
2019-10-22 22:27:05 -0500

    tarea3

--- TAREAS/3/main.c
+++ TAREAS/3/main.c
@@ -1,16 +1,12 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include<stdio.h>
+#include<stdlib.h>
 int main(int argc, char *argu[]){
-	float n1,n2,r1,r2,r3,r4;
-	n1 = atof(argu[1]);
-	n2 = atof(argu[2]);
-	r1 = n1+n2;
-	printf("%.2f\n",r1);
-	r2 = n1-n2;
-	printf("%.2f\n",r2);
-	r3 = n1/n2;
-	printf("%.2f\n",r3);
-	r4 = n1*n2;
-	printf("%.2f\n",r4);
-	return 0;
+    float n1,n2;
+    n1= atof(argu[1]);
+    n2= atof(argu[2]);
+    printf("%.2f\n",n1+n2);
+    printf("%.2f\n",n1-n2);
+    printf("%.2f\n",n1*n2);
+    printf("%.2f\n",n1/n2);
+    return 0;
 }
