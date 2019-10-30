5330cc7f39e1385467974fae547de29b9787f7c6
vanessa <vbazaldua60@gmail.com>
2019-10-30 00:45:01 -0600

    Tarea 2

--- TAREAS/2/main.c
+++ TAREAS/2/main.c
@@ -3,30 +3,15 @@
 
 int main(int argc, char const *argu[]){
 
-<<<<<<< HEAD
-{
-	int numero;
-	numero=atoi(argu[1];
-=======
 	int numero;
 	numero=atoi(argu[1]);
->>>>>>> 1527adf767227071a2ca31705fa09feeb70e293d
 	if (numero%2==0){
 
 		printf("1\n");
 	}
-<<<<<<< HEAD
-	else
-	{
-		printf("0\n");
-	}
-	return 0;
-}		
-=======
 	else {
 	
 		printf("0\n");
 	}
 	return 0;
 }	
->>>>>>> 1527adf767227071a2ca31705fa09feeb70e293d
