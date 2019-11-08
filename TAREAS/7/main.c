97e9b6cfe74d1d59d3b0c2e2945aee0a7cbb5321
vanessa <vbazaldua60@gmail.com>
2019-11-08 00:22:54 -0600

    Tarea 7 y 8

--- TAREAS/7/main.c
+++ TAREAS/7/main.c
@@ -1,13 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 int main(int argc, char* argu[]){
+	//Programa potencia
 	float n1,n2,resultado=1;
+	//declaramos nuestras variables como float 
+	//para que tambien manejen decimales 
 	n1 = atof(argu[1]);
 	n2 = atof(argu[2]);
-
+	//estos son nuestros 2 parametros de entrada 
+	//que convertiremos a float
 	for(int a=1;a<=n2;a++){
 		resultado = resultado*n1;
+	//este for es para acumular la suma de la multiplicacion del parametro que entre 
+	//como potencia por el otro numero que se ingrese;siempre y cuando sea diferente de 1
 	}
-	printf("%.2f\n",resultado);
+	printf("%.2f\n",resultado); //finalmente mostramos resultado
 	return 0;
 }
--- TAREAS/8/main.c
+++ TAREAS/8/main.c
@@ -1,16 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 int main(int argc, char*argu[]){
+	//Programa conversion de grados
 	float n1,n2,resultado;
+	//declaramos nuestras variables como float 
+	//para obtener decimales
 	n1 = atof(argu[1]);
 	n2 = atof(argu[2]);
+	//aqui convertirmos los parametros a flotantes.
 if(n1==0){
 	resultado = (n2*1.8)+32;
 	printf("%.2f\n",resultado);
+	//este if es para que cuando ingrese un 0 con la funcion se covierta 
+	//el segundo parametro de grados celsius a fahrenheit
 }
 if(n1!=0){
 	resultado=(n2-32)/1.8;
-	printf("%.2f\n",resultado);
+	//este por el contrario es por si ingresa uno parametro diferente de 0
+	//convertira de grados fahrenheit a celsius
+	printf("%.2f\n",resultado);//muestra el resultado
 }
 return 0;
 }
